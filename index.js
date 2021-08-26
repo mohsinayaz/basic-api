73ad517a7e63c3c6254715bc8210706ba802c4f6
Mohsin <mohxn.ayaaz@gmail.com>
2021-08-25 19:03:09 -0700

    fix(heroku) heroku port again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const port = procee.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 require("./db");
 
 app.use(express.json());
