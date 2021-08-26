8e1e3165bc3f223125766fefe86c48c7e188edd5
Mohsin <mohxn.ayaaz@gmail.com>
2021-08-25 18:41:41 -0700

    chore(db): connect with mongo cloud

--- db/index.js
+++ db/index.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/notes-db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://localhost:27017/notes-db", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-// mongoose.connect(
-//   "mongodb+srv://mohsin-ayaz:<password>@cluster0.sfx99.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
+const { DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } =
+  process.env;
+
+mongoose.connect(
+  `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/${DATABASE_NAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
